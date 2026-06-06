d08fc6992698c023f91c3ebe4d4d042702f1daef
Raksharth123 <raksharthkohli145@gmail.com>
2026-06-06 19:53:55 +0530

    fix cors

--- server.js
+++ server.js
@@ -26,7 +26,14 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173', 'http://127.0.0.1:5175'],
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'http://127.0.0.1:5173',
+        'http://127.0.0.1:5175',
+        'https://appointy-frontend-9mlq.onrender.com'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token']
